c1cb320ee3b26f91b0628ba04bb8673c83b9a958
PREMBHISE <166536696+PREMBHISE@users.noreply.github.com>
2026-04-02 21:29:04 +0530

    Add files via upload

---
+++ Phase2_Looping_Patterns/Level_2_Number_Based_Looping/PerfectNumberCheck.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class PerfectNumberCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a Number: ");
+        int num = sc.nextInt();
+
+        int sum = 0;
+        for(int i = 1; i<= num/2; i++) {
+            if(num % i == 0) {
+                sum += i;   
+            }
+        }
+
+        if (sum == num ) {
+            System.out.println("This is a Perfect Number");
+        } else {
+            System.out.println("This is not a Perfect Number");
+        }
+        sc.close();
+    }
+}
